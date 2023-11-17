c59709bdfc2c0cb57f220d14daa54e60fc7d40b1
Tyran15 <mh15122006@gmail.com>
2023-11-17 17:23:33 -0300

    Correção do loginCOntroller.php

--- views/user/cadastro.php
+++ views/user/cadastro.php
@@ -7,7 +7,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="../../Nexus/public/css/login.css">
+    <link rel="stylesheet" type="text/css" href="../../public/css/login.css">
     <script src="Script JS/script.js"></script>
     <title>Nexus</title>
 </head>
@@ -15,7 +15,8 @@
     <div class="form-container">
     <button style="background-color: transparent; border: none; display: block; margin: 0 auto;"><a href="/Nexus/views/home/index.php"><img src="/Nexus/public/img/Logo/Nome-Logo.png" alt="imagem" width="200" height="auto" class="img-login"></a></button>
         <h3 class="titulo">Cadastro</h3>
-        <form action="" method="post" class="needs-validation" novalidate>
+        <form action="../../controllers/loginController.php" method="post" class="needs-validation" novalidate>
+            <input type="hidden" name="option" value="INSERT">
             <div class="form-linha">
                 <div class="form-grupo">
                     <div class="form-flutuante">
@@ -79,7 +80,7 @@
             </script>
             <button class="botao mb-3" type="submit">Cadastrar</button>
         </form>
-        <a class="link" href="loginController.php">Já tem uma conta?</a>
+        <a class="link" href="./login.php">Já tem uma conta?</a>
     </div>
     <script src="./validacaoCampos.js"></script>
 </body>
--- views/user/login.php
+++ views/user/login.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="../../Nexus/public/css/login.css">
+    <link rel="stylesheet" type="text/css" href="../../public/css/login.css">
     <title>Nexus</title>
 </head>
 
@@ -13,7 +13,8 @@
     <div class="form-container2">
     <button style="background-color: transparent; border: none; display: block; margin: 0 auto;"><a href="/Nexus/views/home/index.php"><img src="/Nexus/public/img/Logo/Nome-Logo.png" alt="imagem" width="200" height="auto" class="img-login"></a></button>
         <h3 class="titulo">Login</h3>
-        <form action="" method="post">
+        <form action="../../controllers/loginController.php" method="post">
+         <input type="hidden" name="option" value="READ">
         <div class="form-flutuante">
                 <input type="email" class="form-controle inputs" id="email" name="email" maxlength="100" required>
                 <label for="email" class="label-texto">Email</label>
@@ -24,7 +25,7 @@
             </div>
             <button  class="botao mb-3" type="submit">Entrar</button>
         </form>
-        <a class="link" href="cadastroController.php">Não tem uma conta?</a><br>
+        <a class="link" href="./cadastro.php">Não tem uma conta?</a><br>
     </div>
 </body>
 
