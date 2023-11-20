def78eab15d404dd76bfbd4f1163693dd4909ce0
FelipeFLX <felipe.frlima@gmail.com>
2023-11-19 23:54:59 -0300

    Criação e estilização da pagina de descrição de jogos (com avaliações)

--- views/componentes/descJogos.php
+++ views/componentes/descJogos.php
@@ -8,6 +8,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" type="text/css" href="/Nexus/public/css/styleJogos.css">
+    <link rel="shortcut icon" href="/Nexus/public/img/Logo/Logo Nexus Icone.png"/>
+    <script src="/Nexus/public/js/script.js"></script>
     <title>Nexus</title>
 </head>
 
@@ -79,10 +81,55 @@
             </div>
         </div>
         <div class="formAva">
+            <h3 class="subTitu">Jogue e compartilhe! Sua avaliação é valiosa para outros jogadores.</h3>
             <form>
-                
+                <div class="ladoAlado2">
+                    <div>
+                        <p class="textAva">Avaliação:</p>
+                    </div>
+                    <div class="avaliacao">
+                        <input type="radio" id="estrela5" name="estrela" value="5">
+                        <label for="estrela5">&#9733;</label>
+                        <input type="radio" id="estrela4" name="estrela" value="4">
+                        <label for="estrela4">&#9733;</label>
+                        <input type="radio" id="estrela3" name="estrela" value="3">
+                        <label for="estrela3">&#9733;</label>
+                        <input type="radio" id="estrela2" name="estrela" value="2">
+                        <label for="estrela2">&#9733;</label>
+                        <input type="radio" id="estrela1" name="estrela" value="1">
+                        <label for="estrela1">&#9733;</label>
+                    </div>
+                </div>
+                <p class="textAva">Comentário:</p>
+                <textarea id="mensagem" name="mensagem" rows="4"  required></textarea>
             </form>
         </div>
+        <section id="testimonials">
+            <div class="testimonial-box-container">
+                <div class="testimonial-box">
+                    <div class="box-top">
+                        <div class="profile">
+                            <div class="profile-img">
+                                <img src="/Nexus/public/img/Perfil/mhPerfil.jpg" />
+                            </div>
+                            <div class="name-user">
+                                <strong>Matheus Henrique</strong>
+                                <span>@tyran15</span>
+                            </div>
+                        </div>
+                        <div class="reviews">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
+                        </div>
+                    </div>
+                    <div class="client-comment">
+                        <p>Muito legal o joguinho, gostei muito, parabens aos desenvolvedores, jogarei mais vezes, recomendarei aos meus amigos❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️</p>
+                    </div>
+                </div>
+            </div>
     </div>
     <?php
     include '../componentes/rodape.php';
