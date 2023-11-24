66cf6c5d6816f9c2e1d068451691524930896ba6
FelipeFLX <felipe.frlima@gmail.com>
2023-11-24 15:30:31 -0300

    Começo do input de imagem no cadastro

--- views/user/cadastro.php
+++ views/user/cadastro.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="../../public/css/login.css">
     <script src="Script JS/script.js"></script>
     <title>Nexus</title>
@@ -78,7 +78,24 @@
                 $('#cpf').mask('000.000.000-00');
                 });
             </script>
-            <button class="botao mb-3" type="submit">Cadastrar</button>
+            <button type="button" class="botao mb-3" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Prosseguir</button>
+
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content" style="background-color: #111113;">
+                    <div class="modal-header" style="background-color: #111113;">
+                        <h1 class="modal-title fs-5" id="staticBackdropLabel" style="color: white;">Para deixar sua experiência mais interativa adicone uma foto de perfil!</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    </div>
+                    <div class="modal-body" style="background-color: #111113;">
+                    </div>
+                    <div class="modal-footer" style="background-color: #111113;">
+                        <button class="botao mb-3" type="submit">Cadastrar</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            
         </form>
         <a class="link" href="./login.php">Já tem uma conta?</a>
     </div>
